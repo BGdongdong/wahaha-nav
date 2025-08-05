b776f1ca671965527e35e034626707f728b1cf47
dongdong <95842973+BGdongdong@users.noreply.github.com>
2025-08-05 15:07:50 +0800

    12

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import Script from 'next/script'
 export const metadata: Metadata = {
   title: {
     default: '',
-    template: '%s'
+    template: '%s-导航'
   },
   description: 'A modern navigation platform',
   icons: {

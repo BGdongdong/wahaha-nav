2c8c51ab3d3e72164405be781e14462650043f0e
dongdong <95842973+BGdongdong@users.noreply.github.com>
2025-08-07 11:24:10 +0800

    1

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -14,7 +14,7 @@ export function ModeToggle() {
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       className="h-9 w-9"
     >
-      <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun className="h-7 w-7 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-7 w-7 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">切换主题</span>
     </Button>
